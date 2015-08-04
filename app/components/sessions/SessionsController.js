8d841bd323479bcec3cd1348d45be474ce10cd9a
Arnav Gupta <championswimmer@gmail.com>
2015-08-05 00:28:52 +0530

    dialogs based on session url routing
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- app/components/sessions/SessionsController.js
+++ app/components/sessions/SessionsController.js
@@ -7,12 +7,51 @@ var sessionsModule = angular.module('oe.sessions', ['ui.router']);
 
 /* ----------------------------- Sessions List ---------------------------- */
 var singleSession = {};
+var showSession = function(session, event, $mdDialog) {
+    singleSession = session;
+    $mdDialog.show({
+        controller: 'SessionDialogController',
+        templateUrl: 'app/components/sessions/sessiondialog.html',
+        parent: angular.element(document.body),
+        targetEvent: event,
+
+    });
+};
 sessionsModule.config(['$stateProvider', function($stateProvider) {
     $stateProvider.state('sessions', {
         url: '/sessions',
         templateUrl: 'app/components/sessions/sessions.html',
         controller: 'SessionsController'
-    })
+    }).state('sessions.session', {
+        url: '/sessions/:sessionId',
+        onEnter: ['sessionId', '$sessionStorage', 'ApiJsonFactory', '$event', '$mdDialog',
+            function (sessionId, $sessionStorage, ApiJsonFactory, $event, $mdDialog) {
+                console.log("Opening sessions/sessionId");
+            if ($sessionStorage.sessions === null ||
+                typeof($sessionStorage.sessions) == 'undefined')
+            {
+                $sessionStorage.sessions = [];
+            }
+            var Sessions = $sessionStorage.sessions;
+
+            if (Sessions.length === 0) {
+                ApiJsonFactory.getJson('sessions')
+                    .then(function (response) {
+                        Sessions = response.data.sessions;
+                        $sessionStorage.sessions = Sessions;
+                    }, function (error) {
+                        console.error(error);
+                    });
+            }
+            var i;
+            for (i = 0; i < Sessions.length; i++) {
+                if (sessionId === i) {
+                    showSession(Sessions[i], $event, $mdDialog)
+                }
+            }
+        }]
+    });
+
 }]);
 
 sessionsModule.controller('SessionsController',
@@ -43,14 +82,7 @@ sessionsModule.controller('SessionsController',
         };
 
         sc.showSession = function(session, event) {
-            singleSession = session;
-            $mdDialog.show({
-                controller: 'SessionDialogController',
-                templateUrl: 'app/components/sessions/sessiondialog.html',
-                parent: angular.element(document.body),
-                targetEvent: event,
-
-            });
+            showSession(session, event, $mdDialog);
         };
 
     }]);
