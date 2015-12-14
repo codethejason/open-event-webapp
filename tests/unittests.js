0656c30dd70f55ea125e6ea91421f36e8105e248
codethejason <tigergoldfish17@gmail.com>
2015-12-13 22:33:09 -0500

    Included initial test. Does not work

--- tests/unittests.js
+++ tests/unittests.js
@@ -1,17 +1,23 @@
-describe('Filters', function(){ //describe your object type
-    beforeEach(module('MyApp')); //load module
+describe('Sessions', function(){ //describe your object type
+    beforeEach(module('sessionsModule')); //load module
     
-    describe('reverse',function(){ //describe your app name
+    //declare controller
+    var $controller;
+
+    beforeEach(inject(function(_$controller_){ //inject the controller
+        $controller = _$controller_;
+    }));  
     
-        var reverse;    
-        beforeEach(inject(function($filter){ //initialize your filter
-            reverse = $filter('reverse',{});
-        }));
+    describe('Chronological Sort',function(){ //describe your app name
         
-        it('Should reverse a string', function(){  //write tests
-            expect(reverse('rahil')).toBe('lihar'); //pass
-            expect(reverse('don')).toBe('nod'); //pass
-            //expect(reverse('jam')).toBe('oops'); // this test should fail
+        it('should order the sessions chronologically', function(){  //write tests
+            var scope = {};
+            var controller = $controller('sessionsModule', {$scope:$scope});
+            var sessions = $scope.Sessions;
+            expect(sessions[0]).toEqual('Die Abschaffung der Wahrheit');
+            expect(sessions[1]).toEqual('I am as old as the Web and this is what I want.');
+            
+            //after we get the other issue fixed I will create a for loop that checks for every index corresponding to every index on the app
         }); 
     
     });
